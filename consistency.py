d14b004274f83f51d2734f8d8e31076dc1434941
Fabio <4898010+gatto@users.noreply.github.com>
2021-10-26 19:05:13 +0200

    Update consistency.py
    comments to code, visualization of some statistics, reformatting

--- consistency.py
+++ consistency.py
@@ -1,6 +1,9 @@
-from rich import print
-from pathlib import Path
 import csv
+from pathlib import Path
+
+from rich import print
+from rich.console import Console
+from rich.table import Table
 
 mainfile = Path("data/tennis.csv")
 
@@ -9,34 +12,33 @@ with open(mainfile) as csvfile:
     for row in reader:
         for i, miao in enumerate(row.items()):
             print(f"{i}: {miao}")
-        break
+        break  # I'm lazy but this should be improved before delivery
 
 with open(mainfile) as csvfile:
     header = csvfile.readline().split(sep=",")
     print(header)
     header_len = len(header)
-    print(f"Our header is {header_len} attributes long.")
-
 
 col_to_check_integrality = set(header[24:47])
 
+st = {}
+st["tot_values"] = 0
+st["missing"] = 0
+st["tot_rows"] = 0
 
-tot_counter = 0
-counter = 0
-tot_rows = 0
 best_of_set = set()
 with open(mainfile) as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        tot_rows += 1
+        st["tot_rows"] += 1
         # let's check all rows are actually header_len (= 49) attributes long.
         assert len(row) == header_len
         for key, val in row.items():
             # contiamo quanti values ci sono in tutto
-            tot_counter += 1
+            st["tot_values"] += 1
             # contiamo quanti missing values ci sono
             if len(val) == 0:
-                counter += 1
+                st["missing"] += 1
             # controlliamo se la colonna è integer (solo se è una delle colonne in "col_to_check_integrality")
             if key in col_to_check_integrality and val:
                 assert row[key] == f"{int(round(float(row[key])))}.0"
@@ -44,15 +46,25 @@ with open(mainfile) as csvfile:
         if row["best_of"] not in best_of_set:
             best_of_set.add(row["best_of"])
 
+shape = (st["tot_rows"], header_len)
 
-
-print(f"In total, we have {counter} missing values through the entire {mainfile}, which by the way has {tot_counter} values all together.")
-
-print(f"This accounts for a {round(counter/tot_counter*100)}% missing values.")
-
-shape = (tot_rows, header_len)
-print(f"There are {shape[0]} rows and {shape[1]} columns.")
-
-print(f"How many best_of sets we have? {best_of_set}")
+# start of visualization
+table = Table(title=f"Statistics for {mainfile}")
+table.add_column("Shape", justify="left", style="cyan")
+table.add_column("Total values", justify="right", style="green")
+table.add_column("Missing values", justify="right", style="cyan")
+table.add_column("Missing values %", justify="right", style="green")
+table.add_column("Best of sets unique", justify="right", style="cyan")
+table.add_row(
+    f"{shape[0]} rows\n{shape[1]} columns.",
+    f"{st['tot_values']} (including the missing)",
+    f"{st['missing']}",
+    f"{round(st['missing']/st['tot_values']*100)}%",
+    f"{best_of_set}",
+)
+console = Console()
+console.print(table)
+# end of visualization
 
 # let's create the tables schemas
+pass
