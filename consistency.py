651b42eb5d07e389c55a5ee5d01189ba5f82e5e8
Fabio <4898010+gatto@users.noreply.github.com>
2021-10-27 13:14:29 +0200

    Update consistency.py

--- consistency.py
+++ consistency.py
@@ -18,7 +18,6 @@ with open(mainfile) as csvfile:
     header = csvfile.readline().split(sep=",")
     print(header)
     header_len = len(header)
-print(type(header))
 
 col_to_check_integrality = set(header[24:47])
 
@@ -57,7 +56,7 @@ table.add_column("Missing values", justify="right", style="cyan")
 table.add_column("Missing values %", justify="right", style="green")
 table.add_column("Best of sets unique", justify="right", style="cyan")
 table.add_row(
-    f"{shape[0]} rows\n{shape[1]} columns.",
+    f"{shape[0]} rows\n{shape[1]} columns."
     f"{st['tot_values']} (including the missing)",
     f"{st['missing']}",
     f"{round(st['missing']/st['tot_values']*100)}%",
@@ -80,10 +79,26 @@ paths["tournament"] = Path("data/m_tournament.csv")
 paths["player"] = Path("data/m_player.csv")
 paths["match"] = Path("data/m_match.csv")
 
-with open(mainfile) as source:
-    for key, path in paths.items():
-        with open(path, mode="w") as target:
-            target.write(f"{headers[key]}\n")
-            """
+for csv_key, path in paths.items():
+    id_rows_in_csv = set()
+    with open(path, mode="w") as target:
+        target.write(f"{headers[csv_key]}\n")
+        with open(mainfile) as source:
             for row in csv.DictReader(source):
-                pass"""
+                writing = True
+                to_app = ""
+                for row_key, value in row.items():
+                    # check unique ID: # i have to do this case-by-case bc otherwise it takes any _id and fucks everything up
+                    if row_key[-2:] == "id":
+                        if value in id_rows_in_csv:
+                            writing = False
+                        else:
+                            id_rows_in_csv.add(value)
+                    # add only the columns that are required in table
+                    if row_key in headers[csv_key]:
+                        if not to_app:
+                            to_app = value
+                        else:
+                            to_app = f"{to_app},{value}"
+                if writing:
+                    target.write(f"{to_app}\n")
