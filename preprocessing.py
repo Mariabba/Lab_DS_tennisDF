e213115af0ed0be0e63a88b4abf57d41048fb47f
jigok <jigokumls97@gmail.com>
2021-11-17 18:30:15 +0100

    tennis_corrected

--- preprocessing.py
+++ preprocessing.py
@@ -8,59 +8,57 @@ from pathlib import Path
 """Countries_ceck"""
 
 df = pd.read_csv("data/inputs/countries.csv")
-#print(df.info())
+# print(df.info())
 df_1 = pd.read_csv("data/inputs/countryinfo.tsv", sep="\t")
 
-#print("Values in countries.csv, column counrty_name: \n", df['country_name'].unique())
+# print("Values in countries.csv, column counrty_name: \n", df['country_name'].unique())
 
-#print("Value in  countryinfo.tsv , column Country  :\n", df_1['Country'].unique())
+# print("Value in  countryinfo.tsv , column Country  :\n", df_1['Country'].unique())
 
 null_data = df[df.isnull().any(axis=1)]
 
-#print(" Modify the values on the basis of that one that are in Country for having exact matching : ")
+# print(" Modify the values on the basis of that one that are in Country for having exact matching : ")
 df['country_name'] = df['country_name'].replace(
     ['Urugay', 'New Zeland', 'United States of America', 'Great Britain', 'North Macedonia'],
     ['Uruguay', 'New Zealand', 'United States', 'United Kingdom', 'Macedonia'])
 
-#print(df['country_name'].unique())
+# print(df['country_name'].unique())
 
 df.to_csv("data/work/countries_corrected.csv", index=False)
 
-"""
-###Saverio per correggere altezza
-df_tennis = pd.read_csv("data/inputs/tennis.csv", low_memory=False)
-
-cols_winner = ["winner_hand", "winner_ht"]
-
-df_tennis.loc[:, cols_winner, ] = df_tennis.groupby('winner_id')[cols_winner].bfill()
-
-df_tennis.to_csv("data/work/tennis_adj.csv", index=False)
 
 
 
+# aggiusto in tennis i loser rank e winner rank
+
+def missing_values_table(df):
+    mis_val = df.isnull().sum()
+    mis_val_percent = 100 * df.isnull().sum() / len(df)
+    mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+    mis_val_table_ren_columns = mis_val_table.rename(
+        columns={0: 'Missing Values', 1: '% of Total Values'})
+    mis_val_table_ren_columns = mis_val_table_ren_columns[
+        mis_val_table_ren_columns.iloc[:, 1] != 0].sort_values(
+        '% of Total Values', ascending=False).round(1)
+    print("Il Dataframe ha " + str(df.shape[1]) + " colonne.\n"
+                                                  "Ci sono " + str(mis_val_table_ren_columns.shape[0]) +
+          " colonne che hanno missing value.")
+    return mis_val_table_ren_columns
 
 
-
-
-# aggiusto in tennis i loser rank e winner rank (con save)
 df_tennis = pd.read_csv("data/inputs/tennis.csv")
-print(df_tennis.info())
 
-cols_rank_winner = ["winner_rank", "winner_rank_points"]
-cols_rank_loser = ["loser_rank", "loser_rank_points"]
+print(missing_values_table(df_tennis), "\n")
 
+df_tennis.loc[:, "winner_rank"] = df_tennis.groupby(['tourney_level'])['winner_rank'].transform(
+    lambda x: x.fillna(x.mean()))
+df_tennis.loc[:, "winner_rank_points"] = df_tennis.groupby(['tourney_level'])['winner_rank_points'].transform(
+    lambda x: x.fillna(x.mean()))
+df_tennis.loc[:, "loser_rank"] = df_tennis.groupby(['tourney_level'])['loser_rank'].transform(
+    lambda x: x.fillna(x.mean()))
+df_tennis.loc[:, "loser_rank_points"] = df_tennis.groupby(['tourney_level'])['loser_rank_points'].transform(
+    lambda x: x.fillna(x.mean()))
 
-df_tennis.loc[:, cols_rank_winner] = df_tennis.sort_values(by="tourney_date").groupby('winner_id')[
-    cols_rank_winner].bfill()
-df_tennis.loc[:, cols_rank_winner] = df_tennis.sort_values(by="tourney_date").groupby('winner_id')[
-    cols_rank_winner].ffill()
-df_tennis.loc[:, cols_rank_loser] = df_tennis.sort_values(by="tourney_date").groupby('loser_id')[
-    cols_rank_loser].bfill()
-df_tennis.loc[:, cols_rank_loser] = df_tennis.sort_values(by="tourney_date").groupby('loser_id')[
-    cols_rank_loser].ffill()
+print(missing_values_table(df_tennis), "\n")
 
-print(df_tennis[["tourney_level", "winner_rank", "winner_rank_points"]].groupby("tourney_level").agg(
-    {'winner_rank': ['mean', 'size'], 'winner_rank_points': ['mean', 'size']}).sort_values(by=("winner_rank", "mean")))
-
-df_tennis["winner_rank"] = df_tennis[["winner_rank", "tourney_level"]].fillna(value=mean_rank_level["winner_rank", "mean"])["winner_rank"]
-"""
+df.to_csv("data/work/tennis_corrected.csv", index=False)
